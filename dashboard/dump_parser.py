526faddc8125f5f3d672510aec8fcaa3472d23a0
Abel 'Akronix' Serrano Juste <akronix5@gmail.com>
2017-06-26 19:37:04 +0200

    Some fixes in dump_parser
    Fixed dump_parser date time parsing adding buffer capability of expat.
    Removed old parser code.
    Preventing from more than one <sitename> definition
    Other improvements
    Added last sqlite from working lagunanegra succesfully generated with this version of the dump_parser

--- dashboard/dump_parser.py
+++ dashboard/dump_parser.py
@@ -1,93 +1,52 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-import xml.etree.ElementTree as ET
 import xml.parsers.expat
 import sys
 
-Debug = True
-
-
-def xml_to_text(source, target = ""):
-  print("Opening file: " + source )
-  if target:
-    target= open(target,'w',encoding = 'utf-8')
-  else:
-    target= open(source[0:-3]+"txt",'w',encoding = 'utf-8')
-  print("Processing...")
-  target.write(";".join(["page_id","page_title","page_ns","revision_id","timestamp","contributor_id","contributor_name","bytes"]))
-  target.write("\n")
-  for event, elem in ET.iterparse(source, events=("start", "end")):
-
-    if event=="start" and elem.tag.split('}')[1] =="sitename":
-      target.write(elem.text)
-      target.write("\n")
-      prefix = elem.tag.split('}')[0] + '}'
-
-    if event =="end" and elem.tag.split('}')[1] == "page":
-      page_id = elem.find(prefix+'id').text
-      page_title= elem.find(prefix+'title').text.replace(";",'')
-      page_ns = elem.find(prefix+'ns').text
-      print("Parsing page " + page_id )
-      for revision in elem.findall(prefix+'revision'):
-        revision_id = revision.find(prefix+'id').text
-        revision_timestamp = revision.find(prefix+'timestamp').text
-
-        revision_bytes = (revision.find(prefix+'text').get('bytes') if revision.find(prefix+'text').get('deleted') != 'deleted' else '-1')
-        contributor = revision.find(prefix+'contributor')
-        if contributor.find(prefix+'id') != None:
-          revision_contributor_id = contributor.find(prefix+'id').text
-          revision_contributor_name = contributor.find(prefix+'username').text.replace(";",'')
-        else:
-          revision_contributor_id = (contributor.find(prefix+'ip').text if contributor.find(prefix+'ip') != None else 'null')
-          revision_contributor_name = ('Anonymous' if contributor.find(prefix+'ip') != None else 'null')
-        target.write(";".join([page_id,page_title,page_ns,revision_id,revision_timestamp,revision_contributor_id,revision_contributor_name,revision_bytes]))
-        target.write("\n")
-  print("Closing file...")
-  target.close()
-  print("Process completed!")
-
+Debug = False
 
 def xml_to_txt(filename):
-  
+
 
   ### BEGIN xmt_to_txt var declarations ###
   # Shared variables for parser subfunctions:
   ## output_txt, _current_tag, _parent
   ## page_id,page_title,page_ns,revision_id,timestamp,contributor_id,contributor_name,bytes_var
-  
+
   output_txt = None
   _parent = None
   _current_tag = ''
-  page_id = -1
-  page_title = ''
-  page_ns = -1
-  revision_id = -1
-  timestamp = ''
-  contributor_id = -1
-  contributor_name = ''
-  bytes_var = -1
-    
-
+  page_id = page_title = page_ns = revision_id = timestamp = contributor_id = contributor_name = bytes_var = ''
+  site_name = None
+  
   def start_tag(tag, attrs):
     nonlocal output_txt, _current_tag, _parent
     nonlocal bytes_var
-    
+
     _current_tag = tag
-    
+
     if tag == 'text':
-      bytes_var = attrs['bytes']
+      if 'bytes' in attrs:
+        bytes_var = attrs['bytes']
+      else: # There's a 'deleted' flag or no info about bytes of the edition
+        bytes_var = '-1'
     elif tag == 'page' or tag == 'revision' or tag == 'contributor':
       _parent = tag
+    
+    if tag == 'upload':
+      print("!! Warning: '<upload>' element not being handled")
 
   def data_handler(data):
     nonlocal output_txt, _current_tag, _parent
     nonlocal page_id,page_title,page_ns,revision_id,timestamp,contributor_id,contributor_name
-    
-    if _current_tag == '': # Don't process blank "orphan" data between tags!! 
+    nonlocal site_name
+
+    if _current_tag == '': # Don't process blank "orphan" data between tags!!
       return
-    
-    if _current_tag == 'sitename':
+
+    if site_name == None and _current_tag == 'sitename':
+      site_name = data
       output_txt.write(data + '\n')
     elif _parent:
       if _parent == 'page':
@@ -95,13 +54,15 @@ def xml_to_txt(filename):
           page_title = data
         elif _current_tag == 'id':
           page_id = data
+          if Debug:
+            print("Parsing page " + page_id )
         elif _current_tag == 'ns':
           page_ns = data
       elif _parent == 'revision':
         if _current_tag == 'id':
           revision_id = data
         elif _current_tag == 'timestamp':
-          timestamp = str(data)
+          timestamp = data
       elif _parent == 'contributor':
         if _current_tag == 'id':
           contributor_id = data
@@ -114,7 +75,7 @@ def xml_to_txt(filename):
   def end_tag(tag):
     nonlocal output_txt, _current_tag, _parent
     nonlocal page_id,page_title,page_ns,revision_id,timestamp,contributor_id,contributor_name,bytes_var
-    
+
     # uploading one level of parent if any of these tags close
     if tag == 'page':
       _parent = None
@@ -122,37 +83,32 @@ def xml_to_txt(filename):
       _parent = 'page'
     elif tag == 'contributor':
       _parent = 'revision'
-    
+
     # print revision to revision output txt
     if tag == 'revision':
       #revision_row = list(map(str,[page_id,page_title,page_ns,revision_id,timestamp,contributor_id,contributor_name,bytes_var]))
       revision_row = [page_id,page_title,page_ns,revision_id,timestamp,contributor_id,contributor_name,bytes_var]
       output_txt.write(";".join(revision_row) + '\n')
       if Debug:
-        print(";".join(revision_row) + '\n')
-      # Cleaning data that has to be recalculated for hygiene and better debugging:
+        print(";".join(revision_row))
+      # Cleaning data that has to be recalculated in every row for hygiene and better debugging:
       revision_id = timestamp = contributor_id = contributor_name = bytes_var = ''
-      
+
     _current_tag = '' # Very important!!! Otherwise blank "orphan" data between tags remain in _current_tag and trigger data_handler!! >:(
-    
+
 
   ### BEGIN xml_to_txt body ###
-    
+
   # Initializing xml parser
   parser = xml.parsers.expat.ParserCreate()
   input_file = open(filename, 'rb')
-  
-  #~ x = 0
-  #~ for l in input_file:
-    #~ print l
-    #~ x = x +1
-    #~ if x > 10:
-      #~ 0/0
 
   parser.StartElementHandler = start_tag
   parser.EndElementHandler = end_tag
   parser.CharacterDataHandler = data_handler
-  
+  parser.buffer_text = True
+  parser.buffer_size = 1024
+
   # writing header for output txt file
   output_txt = open(filename[0:-3]+"txt",'w')
   output_txt.write(";".join(["page_id","page_title","page_ns","revision_id","timestamp","contributor_id","contributor_name","bytes"]))
